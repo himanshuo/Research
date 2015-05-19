0fbf6f7c98552a24d2ba7b13ffee6bd80de1b57d
himanshu <ho2es@virgina.edu>
2015-05-19 01:20:06 -0400

    send emails but keep continuing when json.loads fails.

--- twitter/get_data.py
+++ twitter/get_data.py
@@ -41,7 +41,15 @@ class DBListener(StreamListener):
 
 
     def on_data(self, data):
-        data = json.loads(data)
+
+        try:
+            data = json.loads(data)
+        except:
+            message="json.loads(data) broke.\n"
+            message += str(data)
+            message += "\n"
+            self.send_emails(title="EXCEPTION", content=message)
+            return True # have it continue.
 
         if 'limit' in data and 'track' in data['limit']:
             return True # ignore current, continue to next tweet.
