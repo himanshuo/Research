cea20cd10e3c4ea2b3b2376d8b9a38955ff6d2ef
himanshu <ho2es@virginia.edu>
2016-02-29 13:48:41 -0500

    filter by two weeks ago

--- second_semester/scrape_new.py
+++ second_semester/scrape_new.py
@@ -10,6 +10,7 @@ import sys
 import functions
 from TwitterSearch import *
 from constants import *
+import datetime
 
 tokenizer = nltk.tokenize.treebank.TreebankWordTokenizer()
 keys = [ts1,ts2,ts3,ts4,ts5,ts6,ts7,ts8,ts9,ts10]
@@ -182,6 +183,7 @@ def add_tweets_to_db(tweets):
         # this will likely break for whatever reasons.
         # ignore tweets that break
         try:
+            # import pdb;pdb.set_trace()
             add_tweet_to_db(tweet)
         except:
             print("failed to add: {}".format(str(tweet)))
@@ -212,24 +214,28 @@ def add_tweet_to_db(tweet):
     tweet_retweets = str(tweet['retweeted_status']['retweet_count'])
     tweet_favorites = str(tweet['retweeted_status']['favorite_count'])
     tweet_twitterid = str(tweet['retweeted_status']['id'])
+    # tweet_created_at = str(tweet['retweeted_status']['created_at'])
+    created_at_datetime = datetime.datetime.strptime(tweet['retweeted_status']['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
+    tweet_created_at = (created_at_datetime-datetime.datetime(1970,1,1)).total_seconds()
+
 
     #add associate user
     upsert_user(username_id, username_name, username_followers)
     # add tweet into db
-    cursor.execute("INSERT INTO Tweet (username,text,retweet_count,favorite_count,twitterID) Values(" + username_id + ",?," + tweet_retweets + "," + tweet_favorites + "," + tweet_twitterid + ")",(tweet_text,))
+    cursor.execute("INSERT INTO Tweet (username,text,retweet_count,favorite_count,twitterID,created_at) Values(" + username_id + ",?," + tweet_retweets + "," + tweet_favorites + "," + tweet_twitterid +","+str(tweet_created_at)+")",(tweet_text,))
     conn.commit()
 
     # add hashtags to db
     branching_tags = create_branching_tags(tweet)
     add_new_tags_to_db(branching_tags)
 
+    print(tweet_text)
+
 
 def tag_query(active_tags):
     global keys
     tweets = []
     for tag in active_tags:
-        # scrape every 5 seconds
-        time.sleep(5)
         # build hashtag in proper format
         hashtag = "#" + str(tag)
 
@@ -250,14 +256,30 @@ def tag_query(active_tags):
             time.sleep(10)
     return tweets
 
+
+def get_two_week_old_tweet_id():
+
+    two_weeks_ago = (datetime.datetime.now() - datetime.timedelta(days=20))
+    epoch = datetime.datetime.utcfromtimestamp(0)
+    two_weeks_ago_seconds_since_epoch = (two_weeks_ago - epoch).total_seconds() * 1000.0
+
+    try:
+        for i in cursor.execute("SELECT twitterID,created_at from tweet WHERE created_at >= ? order by created_at limit 1", (two_weeks_ago_seconds_since_epoch,)):
+            return i[0]
+    except:
+        return 0
+
 def user_query(active_users):
+    global keys
     tweets = []
     for user in active_users:
         user = str(user[2])
         try:
             tuo = TwitterUserOrder(user)
+            tuo.set_since_id(get_two_week_old_tweet_id())
             # todo: search_tweets_iterable gets all historical tweets of user in chunks. Perhaps stop getting more chunks if x condition
-            tweets += ts.search_tweets_iterable(tuo)
+            ts = keys[0]
+            tweets = ts.search_tweets_iterable(tuo)
         except Exception as e:
             # if fail, move key[0] to end and sleep to allow key to reset
             keys = keys[1:] + [keys[0]]
@@ -268,7 +290,6 @@ def user_query(active_users):
 def scrape_by_tag():
     # determine which tags to look for. tags that havent already been checked, order by decreaseing number of references to them
     active_tags = cursor.execute("SELECT * from Hashtag where checked == 0 ORDER BY ref DESC limit 1")
-    time.sleep(5)
     active_tag_strings = [ str(tag[1]) for tag in active_tags]
     tweets = tag_query(active_tag_strings)
     tweets = filter_tweets(tweets)
@@ -278,7 +299,9 @@ def scrape_by_tag():
 
 def scrape_by_user():
     active_users = cursor.execute("SELECT * from Username WHERE checked == 0 limit 1")
+    import pdb;pdb.set_trace()
     tweets = user_query(active_users)
+
     tweets = filter_tweets(tweets)
     add_tweets_to_db(tweets)
 
@@ -295,9 +318,10 @@ def init():
     #     if data[0] == 0: # no initial hashtags
 
 
+
 # MAIN
 if __name__=="__main__":
     init()
     while True:
-        scrape_by_tag()
+        # scrape_by_tag()
         scrape_by_user()
