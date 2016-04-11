0cc2d32b5a1de2a981f03f2b4c34bd82967b6aae
himanshu <himanshuo@gmail.com>
2016-04-11 12:59:17 -0400

    create position vector code

--- second_semester/create_pos_vector.py
+++ second_semester/create_pos_vector.py
@@ -24,6 +24,78 @@ tokenizer = nltk.tokenize.treebank.TreebankWordTokenizer()
 tweets = db_read.execute("SELECT * from Tweet")
 temp_tweets = []
 
+POS_INDEX = {
+    'CC':0,
+    'CD':1,
+    'DT':2,
+    'EX':3,
+    'FW':4,
+    'IN':5,
+    'JJ':6,
+    'JJS':7,
+    'LS': 8,
+    'MD': 9,
+    'NN': 10,
+    'NNS': 11,
+    'NNP': 12,
+    'NNPS': 13,
+    'PDT': 14,
+    'POS': 15,
+    'PRP': 16,
+    'PRP$': 17,
+    'RB': 18,
+    'RBR': 19,
+    'RBS': 20,
+    'RP': 21,
+    'TO': 22,
+    'UH': 23,
+    'VB': 24,
+    'VBD': 25,
+    'VBG': 26,
+    'VBN': 27,
+    'VBP': 28,
+    'VBZ': 29,
+    'WDT': 30,
+    'WP': 31,
+    'WP$': 32,
+    'WRB':33,
+    'NOUN':34,
+    'VERB':35,
+    'ADJECTIVE':36,
+    'PRONOUN':37,
+    'ADVERB': 38,
+    'JJR':39,
+    '.':40,
+    ':':41,
+    ',':42,
+    '(':43,
+    ')':44,
+    '\'\'':45,
+    ';':46,
+}
+
+def get_pos_features(tagger):
+    out = [0 for i in range(0,len(POS_INDEX))]
+
+    for t in tagger:
+        try:
+            index = POS_INDEX[t[1].upper()]
+            out[index] += 1
+            if t[1].upper() in ['NN', 'NNS', 'NNP', 'NNPS']:
+                out[POS_INDEX['NOUN']] += 1
+            elif t[1].upper() in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']:
+                out[POS_INDEX['VERB']] += 1
+            elif t[1].upper() in ['JJ', 'JJR', 'JJS']:
+                out[POS_INDEX['ADJECTIVE']] += 1
+            elif t[1].upper() in ['PRP', 'PRP$', 'WP','WP$']:
+                out[POS_INDEX['PRONOUN']] += 1
+            elif t[1].upper() in ['RB', 'RBR', 'RBS', 'WRB']:
+                out[POS_INDEX['ADVERB']] += 1
+        except:
+            print('not adding {} because punctuation'.format(t[1]))
+    out = str(out)[1:-1]
+    return out
+
 CHUNK_SIZE = 1000
 def process_chunk(tweets):
     process_list = []
@@ -64,7 +136,12 @@ def process_chunk(tweets):
         length = tweets_map[x]
         tagged = tagged1[:length]
         tagged1 = tagged1[length:]
-        db_read.execute("insert into FullVector(tweet_id, full_vector) values (?, ?);", [x, str(tagged)])
+        pos_features = get_pos_features(tagged)
+        print('\n ')
+        print(tagged)
+        print(pos_features)
+        print(' \n')
+        db_read.execute("insert into FullVector(tweet_id, full_vector) values (?, ?);", [x, pos_features])
 
         # if str(tagged) != str(temp_map[x]):
         #     print('bad')
